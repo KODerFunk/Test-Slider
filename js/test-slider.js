917139cf54291825e7f307dfd12692f9ae9dab8e
ZaitcevIlya <intiguan@yahoo.com>
2015-03-15 19:19:32 +0300

    верстку закончил. осталось прицепить пагинацию, и цвета кнопок, текста.

--- js/test-slider.js
+++ js/test-slider.js
@@ -1,8 +1,9 @@
 var slider = {
-  slides: ['image/img-1.png', 'image/img-2.png', 'image/img-1.png', 'image/img-2.png', 'image/img-1.png', 'image/img-2.png'],
+  slides: ['image/img-1.jpg', 'image/img-2.jpg', 'image/img-3.jpg', 'image/img-4.jpg', 'image/img-5.jpg', 'image/img-6.jpg'],
   frame: 0,
   set: function(image) {
     document.getElementById('src').style.backgroundImage = 'url('+image+')';
+    document.getElementsByClassName()
   },
   init: function() {
       this.set(this.slides[this.frame]);
